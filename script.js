d78a843ea7a4fc8fa13b41beaa5abbdf08a660cb
Wills <ejioguvictor6@gmail.com>
2022-08-01 00:54:14 +0100

    last commit for the project

--- script.js
+++ script.js
@@ -3,12 +3,27 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// show loading 
+const loading = () => {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// hide loading
+const complete = () => {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // show new quote randomly
 const newQuote = () =>{
+    // show loading before any content
+    loading();
+
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // checking for blank authors and replacing with unknown.
     if(!quote.author){
@@ -23,11 +38,15 @@ const newQuote = () =>{
     } else {
         quoteText.classList.remove('long-quote');
     }
+
+    // set quote and hide loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // get quotes from API
 async function getQuotes () {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -38,6 +57,16 @@ async function getQuotes () {
         }
 }
 
+
+// activating the tweet button
+const tweetQuote = () => {
+    const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterURL, '_blank');
+}
+
+// event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // to run the function once page loads
 getQuotes();
